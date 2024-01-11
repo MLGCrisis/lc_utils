55af688e47a1e0e96177d5f418b56b88621333c2
GG79 <midlifegamingcrisis@gmail.com>
2024-01-11 00:38:37 -0800

    config edits

--- config.lua
+++ config.lua
@@ -19,11 +19,11 @@ Config.format = {
 -- The "default" option will use the framework's default script
 -- ATTENTION: If you set it to "other," it's necessary to configure the script inside the custom_scripts folder in the respective file
 Config.custom_scripts_compatibility = {
-	['fuel'] = "default",						-- [ox_fuel|ps-fuel|sna-fuel|cdn-fuel|LegacyFuel|default|other] Fuel script Compatibility
+	['fuel'] = "ps-fuel",						-- [ox_fuel|ps-fuel|sna-fuel|cdn-fuel|LegacyFuel|default|other] Fuel script Compatibility
 	['inventory'] = "default",					-- [ox_inventory|qs-inventory|ps-inventory|default|other] Inventory script Compatibility
 	['keys'] = "default",						-- [qs-vehiclekeys|cd_garage|jaksam|wasabi_carlock|default|other] Keys script Compatibility
-	['mdt'] = "default",						-- [ps-mdt|default|other] MDT script Compatibility (to log weapon serial)
-	['target'] = "disabled",					-- [qb-target|ox_target|disabled|other] Target script Compatibility (disabled will use markers)
+	['mdt'] = "ps-mdt",						-- [ps-mdt|default|other] MDT script Compatibility (to log weapon serial)
+	['target'] = "qb-target",					-- [qb-target|ox_target|disabled|other] Target script Compatibility (disabled will use markers)
 	['notification'] = "default",				-- [okokNotify|default|other] Notification script Compatibility
 }
 
@@ -53,7 +53,7 @@ Config.spawned_vehicles = {						-- Config for the vehicles that are spawned in 
 }
 
 Config.owned_vehicles = {							-- (Only for the vehicles that will be inserted into your garage) This is the config to insert a vehicle in the player owned vehicles table
-	['plate_format'] = 'xxxxxxxx',					-- Plate generation format. [n = number | l = letter | x = any]
+	['plate_format'] = 'lll nnnn',					-- Plate generation format. [n = number | l = letter | x = any]
 	['default'] = {									-- The garage type
 		['garage'] = 'motelgarage',					-- This is the garage where the owned vehicles will be inserted
 		-- ['garage'] = 'SanAndreasAvenue', 		-- Default garage for ESX framework
